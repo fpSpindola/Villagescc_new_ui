768442ad6c2058cbaad474bbe26a6c7c36c796b4
Filipe Spindola <filipe.spindola@hotmail.com>
2017-04-26 02:27:02 -0300

    refactoring posting display after login

---
+++ accounts/migrations/0003_auto_20170426_0241.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0002_auto_20161212_1238'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='profile',
+            name='user',
+        ),
+        migrations.DeleteModel(
+            name='Profile',
+        ),
+    ]
