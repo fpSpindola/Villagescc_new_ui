4b0109b6cf28f806814802eab14e9f30270a4108
Filipe Spindola <filipe.spindola@hotmail.com>
2017-05-21 00:32:23 -0300

    tools folder

---
+++ tools/listing_migration.py
@@ -0,0 +1,28 @@
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "ccproject.settings"
+
+import django
+
+django.setup()
+
+from post.models import Post
+from listings.models import Listings
+
+all_posts = Post.objects.all()
+for post in all_posts:
+    post_user_id = post.user_id
+    post_date = post.date
+    if len(post.title) < 220:
+        post_title = post.title
+    else:
+        post_title = post.title[:220]
+
+    post_text = post.text
+    post_image = post.image
+    post_listing_type = 'REQUEST'
+
+    listing = Listings(title=post_title, listing_type=post_listing_type, photo=post_image, user_id=post_user_id,
+                       subcategories_id=2, description=post_title, created=post_date, updated=post_date)
+    listing.save()
+    print('Listing saved')
