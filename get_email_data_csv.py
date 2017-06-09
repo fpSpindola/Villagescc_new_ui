a7654daa8689e66709a18b3923b8ad1c29f9b0a6
Filipe Spindola <filipe.spindola@hotmail.com>
2017-06-09 01:46:43 -0300

    script to export profiles data to csv

---
+++ get_email_data_csv.py
@@ -0,0 +1,32 @@
+import os
+import django
+import csv
+import geopy
+import googlemaps
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "ccproject.settings"
+
+django.setup()
+
+from profile.models import Profile
+from django.contrib.auth.models import User
+
+
+profiles = Profile.objects.all()
+writer = csv.writer(open("profiles.csv", "ab"), delimiter=',', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['name, email, city, state, neighborhood'])
+for each_profile in profiles:
+    profile_data = [each_profile.name, each_profile.email, each_profile.location.city, each_profile.location.state,
+                    each_profile.location.neighborhood]
+    try:
+        writer.writerow(profile_data)
+    except Exception as e:
+        try:
+            profile_data = [each_profile.name, each_profile.email]
+            writer.writerow(profile_data)
+        except Exception as e:
+            print(e)
+
+
+
+# reader = csv.reader(open("friends.csv", "rb"), delimiter=' ')
