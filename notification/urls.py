ce1012f590be748036133c4f023a75f56cf9638b
filipe <filipe.spindola@psafe.com>
2017-06-26 14:03:12 -0300

    fixed mapping and now passing lat and lon as a float

--- notification/urls.py
+++ notification/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 import notification.views as notification_views
 
 urlpatterns = [
-    url(r'^in_construction/', notification_views.notification_soon, name="notification_soon"),
+    url(r'^notifications/', notification_views.notification, name="notification"),
 ]
--- notification/views.py
+++ notification/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from notification.models import Notification
 
 
-def notification_soon(request):
-    return render(request, 'in_construction.html')
+def notification(request):
+    unread_notifications = Notification.objects.filter(recipient=request.profile, status=Notification.NEW).all()
+    Notification.objects.filter(recipient=request.profile).update(status=Notification.READ)
+    return render(request, 'notifications.html', {'unread_notifications': unread_notifications})
