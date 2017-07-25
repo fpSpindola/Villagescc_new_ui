6f4846349d4bae10d4c52d3b15e5b3f2b70dca25
Filipe Spindola <filipe.spindola@hotmail.com>
2017-07-25 20:40:04 -0300

    debug true

--- get_email_data_csv.py
+++ get_email_data_csv.py
@@ -1,6 +1,8 @@
 import os
 import django
 import csv
+import geopy
+import googlemaps
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "ccproject.settings"
 
@@ -14,16 +16,19 @@ profiles = Profile.objects.all()
 writer = csv.writer(open("profiles.csv", "ab"), delimiter=',', quoting=csv.QUOTE_MINIMAL)
 writer.writerow(['name, email, city, state, neighborhood'])
 for each_profile in profiles:
-    profile_data = [each_profile.name, each_profile.email, each_profile.location.city, each_profile.location.state,
-                    each_profile.location.neighborhood]
     try:
+        profile_data = [each_profile.name,
+                        each_profile.email,
+                        each_profile.location.city,
+                        each_profile.location.state,
+                        each_profile.location.neighborhood]
+        writer.writerow(profile_data)
+        profile_data = [each_profile.name, each_profile.email]
         writer.writerow(profile_data)
     except Exception as e:
-        try:
-            profile_data = [each_profile.name, each_profile.email]
-            writer.writerow(profile_data)
-        except Exception as e:
-            print(e)
+        print(e)
+
+
 
 
 
